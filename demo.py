db2e2e83e866435c03a1942a67d4f8671e52840b
cqyizhong <52626179+cqyizhong@users.noreply.github.com>
2019-07-08 21:23:14 +0800

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,18 @@
+from turtle import *
+
+pensize(1)
+sc = Screen()
+
+
+print(pos())
+goto(100,100)
+print(pos())
+goto(100,-100)
+print(pos())
+goto(-100,-100)
+print(pos())
+goto(-100,100)
+print(pos())
+goto(0,0)
+print(pos())
+done()
